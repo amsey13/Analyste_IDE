eeed7a6a5623aac6dcc17a9fd77c7d742a146992
Glauriel <glauriel-fosther.badjila-wandja-legara.etu>
2026-03-06 12:41:19 +0100

    adding of attributes in SupportProjects

--- Backend/src/main/java/com/example/backend/acc/dto/SupportProjectRequestDTO.java
+++ Backend/src/main/java/com/example/backend/acc/dto/SupportProjectRequestDTO.java
@@ -4,5 +4,15 @@ import com.example.backend.core.modules.projects.dto.BaseProjectRequestDTO;
 
 public class SupportProjectRequestDTO extends BaseProjectRequestDTO {
 
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
     //Maybe one day they'll be something there ...
 }
