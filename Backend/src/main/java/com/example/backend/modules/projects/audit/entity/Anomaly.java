f72f7d8c90019e9d2055374fb1579e5bb6595c7c
Glauriel <glauriel-fosther.badjila-wandja-legara.etu>
2026-03-12 14:32:05 +0100

    fix error Anomaly entity

--- Backend/src/main/java/com/example/backend/modules/projects/audit/entity/Anomaly.java
+++ Backend/src/main/java/com/example/backend/modules/projects/audit/entity/Anomaly.java
@@ -1,7 +1,19 @@
 package com.example.backend.modules.projects.audit.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Anomaly {
+
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
