21a4a2c12faf281ddec1a9f4e491451348c8ad23
Glauriel <glauriel-fosther.badjila-wandja-legara.etu>
2026-03-16 00:23:22 +0100

    adding a properties files for integration test

--- Backend/src/test/java/com/example/backend/integration/AuditIntegrationTest.java
+++ Backend/src/test/java/com/example/backend/integration/AuditIntegrationTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 public class AuditIntegrationTest {
 
 
